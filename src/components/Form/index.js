043acc5fbdb13767dcb3c71ad5d59d3fd76aeec1
Ross Westwater <ross.westwater@gmail.com>
2022-05-20 18:17:57 -0600

    working on historical rendering. will refactor/style when complete

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,64 +1,90 @@
-import { useState, useEffect } from 'react';
-import API_Key from '../../assets/secret'
+import { useState, useEffect } from "react";
+import API_Key from "../../assets/secret";
+import History from "../History";
 
 function Form(props) {
-  const [ question, setQuestion ] = useState('')
-  const [ answer, setAnswer ] = useState('')
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [history, setHistory] = useState([]);
+  const slicedHist = history.slice(1)
 
   useEffect(() => {
-    console.log('updated state', question);
-    console.log('updated state', answer);
-  }, [question, answer])
+    // console.log("updated prompt", question);
+    // console.log("updated answer", answer);
+    // console.log("updated history", history);
+  }, [question, answer, history]);
 
   const handleSubmit = async (e) => {
-    let prompt = document.getElementById('question').value
+    let prompt = document.getElementById("question").value;
     e.preventDefault();
-    
-    await console.log(API_Key);
-    const response = await fetch(`https://api.openai.com/v1/engines/text-curie-001/completions`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_Key}`
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: 20,
-        temperature: 1,
-        n: 2
-      })
-    })
-    let reply = await response.json()
-    await setQuestion(prompt)
-    await setAnswer(reply.choices)
-    await console.log('reply',reply);
-    await console.log('answer',answer);
-  }
+
+    const response = await fetch(
+      `https://api.openai.com/v1/engines/text-curie-001/completions`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_Key}`,
+        },
+        body: JSON.stringify({
+          prompt: prompt,
+          max_tokens: 5,
+          temperature: 1,
+          n: 2,
+        }),
+      }
+    );
+    let reply = await response.json();
+    await setQuestion(prompt);
+    await setAnswer(reply.choices);
+    await setHistory([
+      { question: prompt, answers: reply.choices },
+      ...history,
+    ]);
+    await console.log(reply);
+  };
 
   return (
     <form id="completion" onSubmit={handleSubmit}>
       <div>
         <label htmlFor="question">Ask a question:</label>
-        <br/>
+        <br />
         <input id="question" type="text"></input>
       </div>
       <button type="submit">Submit</button>
-      {!question && !answer ? null : 
-      <div> You asked: {question}
-      <br/>
-      Her response:
-      <ul>
-        {answer.map((answer) => {
-          return (
-            <li>{answer.text}</li>
-          )
-        })
-        }
-      </ul>
-      </div>
-      }
+      {!question && !answer ? null : (
+        <main>
+          <div>
+            You asked: {history[0].question}
+            <br />
+            Her response:
+            <ul>
+              {history[0].answers.map((answer) => {
+                return <li key={answer.index}>{answer.text}</li>;
+              })}
+            </ul>
+          </div>
+          <section>
+            Previous Queries:
+            {slicedHist.map((input) => {
+              console.log(input);
+              return (
+                <div >
+                  <div>You: {input.question}</div>
+                  <ul>
+                    Her:
+                    {input.answers.map((answer) => {
+                      return <li key={answer.index}>{answer.text}</li>;
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
+          </section>
+        </main>
+      )}
     </form>
-  )
+  );
 }
 
 export default Form;
